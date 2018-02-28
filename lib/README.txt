b0cbab0e08ce1b1f1a7d066b8c8975b6bf5bcfad
Forrest Brennen <forrest.brennen@nanoporetech.com>
2018-02-28 15:54:46 +0000

    Update to version 2.7 of minimap2

---
+++ lib/README.txt
@@ -0,0 +1,4 @@
+This folder is intended to take the ont_minimap2 project's library files.
+Call cmake with the "install" target to have them built and copied
+here.
+This is also the folder the tests are run from when calling "ctest".
--- ont_minimap2.c
+++ ont_minimap2.c
@@ -16,6 +16,13 @@ KSEQ_INIT(gzFile, gzread)
 
 #include "ont_minimap2.h"
 
+// These are prototypes of methods in minimap2's index.c
+int64_t mm_idx_is_idx(const char *fn);
+mm_idx_t *mm_idx_gen(mm_bseq_file_t *fp, int w, int k, int b,
+		     int flag, int mini_batch_size, int n_threads,
+		     uint64_t batch_size);
+mm_idx_t *mm_idx_load(FILE *fp);
+
 static inline void str_enlarge(kstring_t *s, int l) {
   // copy from format.c:7
   if (s->l + l + 1 > s->m) {
@@ -34,10 +41,31 @@ static inline void str_copy(kstring_t *s, const char *st, const char *en) {
 
 /// loads index and returns a pointer to it. Call ontmm_unload_index() to free  the return value.
 mm_idx_t *ontmm_load_index(const char *index_filename) {
-  // see main.c:190sq.
-  FILE *fpr = fopen(index_filename, "rb");
-  mm_idx_t *mi = mm_idx_load(fpr);
-  fclose(fpr);
+  // disable printing to stdout
+  mm_verbose = 0;
+
+  // see main.c:225sq.
+  int is_idx = mm_idx_is_idx(index_filename);
+  if (is_idx < 0) return NULL;
+
+  FILE *fpr = NULL;
+  mm_bseq_file_t *fp = NULL;
+  if (is_idx && !(fpr = fopen(index_filename, "rb"))) return NULL;
+  if (!is_idx) fp = mm_bseq_open(index_filename);
+
+  // parameters for index generation
+  int k = 15, bucket_bits = 14, n_threads = 3, is_hpc = 0, keep_name = 1;
+  int w = (int)(.6666667 * k + .499);
+  int minibatch_size = 200000000;
+  uint64_t batch_size = 4000000000ULL;
+
+  mm_idx_t *mi = NULL;
+  if (fpr) mi = mm_idx_load(fpr);
+  else if (fp) mi = mm_idx_gen(fp, w, k, bucket_bits, is_hpc, minibatch_size,
+                               n_threads, batch_size);
+
+  if (fpr) fclose(fpr);
+  if (fp) mm_bseq_close(fp);
   return mi;
 }
 
@@ -47,6 +75,7 @@ mm_idx_t *ontmm_load_index(const char *index_filename) {
 char *ontmm_align(mm_bseq1_t query, const mm_idx_t *index) {
   // disable printing to stdout
   mm_verbose = 0;
+
   // see example.c:22
   mm_mapopt_t opt;
   mm_mapopt_init(&opt); // initialize mapping parameters
@@ -63,7 +92,7 @@ char *ontmm_align(mm_bseq1_t query, const mm_idx_t *index) {
   kstring_t sam_string;
   sam_string.l = sam_string.m = 0, sam_string.s = 0;
   if (!n_reg) {
-    char *empty_sam = "\t4\t*\t0\t0\t*\t*\t0\t0";
+    char *empty_sam = "\t4\t*\t0\t0\t*\t*\t0\t0\n";
     str_copy(&sam_string, query.name, query.name + strlen(query.name));
     str_copy(&sam_string, empty_sam, empty_sam + strlen(empty_sam));
   }
@@ -71,12 +100,16 @@ char *ontmm_align(mm_bseq1_t query, const mm_idx_t *index) {
     // traverse hits and print them out
     for (j = 0; j < n_reg; ++j) {
       const mm_reg1_t *r = &reg[j];
+      #ifndef NDEBUG
       printf("[ont_minimap2 lib] Score: %d\n", r->score);
+      #endif
       if (!r->p) return strdup("ERROR:alignment has not returned cigar string.");
       kstring_t sam_line;
       sam_line.l = sam_line.m = 0, sam_line.s = 0;
       mm_write_sam(&sam_line, index, &query, r, n_reg, reg);
+      #ifndef NDEBUG
       printf("[ont_minimap2 lib] SAM line: %s\n", sam_line.s);
+      #endif
       str_copy(&sam_string, sam_line.s, sam_line.s + sam_line.l);
       char* newline = "\n";
       str_copy(&sam_string, newline, newline+1);
